11073f3af5ab6fddf25c8882af0e48a5ca3a77e8
Farooq Iqbal <farooqiqbal1209@gmail.com>
2024-12-02 15:36:37 -0800

    Add project files.

---
+++ SerialProgressSeeker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SerialProgressSeeker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Farooq!");
+        }
+    }
+}
